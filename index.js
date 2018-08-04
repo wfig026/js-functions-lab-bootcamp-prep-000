d13b8a5b70100d91170133eba14989a11e1f61e2
Will Figueiredo <will@wfigueiredo.com>
2018-08-04 16:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+name = "Jane"
+
+function happyHolidayTo(holiday, name) {
+  if (holiday === undefined && name === undefined) {
+    holiday = "Independence Day"
+    name = "Jane"
+  }
+  return `Happy ${holiday}, ${name}!`
+}
