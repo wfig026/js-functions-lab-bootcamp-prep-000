489e76ab432d8b13f71030dfa2210498a7b01d20
Will Figueiredo <will@wfigueiredo.com>
2018-08-04 16:39:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // write your code below!
+
+// Return value "Happy holidays"
 function happyHolidays() {
   return "Happy holidays!"
 }
 
+// Return "Happy holidays, 'name'!"
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 name = "Jane"
 
+// Return "Happy 'holiday', 'name'!"
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 holiday = "Independence Day"
 name = "Jane"
+
+// Return "It's 'x' days until 'holiday'!"
+function holidayCountdown(holiday, days) {
+  
+}
